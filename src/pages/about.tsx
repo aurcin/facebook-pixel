6bdd18e1d69990cc2e25a6a247991999b11155a2
Aurimas Cincikas <aurcin@gmail.com>
2025-06-17 17:44:03 +0300

    added events

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,17 @@
 function AboutPage() {
+  function handleClick() {
+    window.fbq?.('trackCustom', 'Event_2_Clicked');
+  }
+
   return (
     <>
       <h2 className='mt-8 text-xl font-medium'>About Page</h2>
       <p className='mt-2 text-neutral-800'>
         The button below is to trigger another custom event
       </p>
-      <button className='button button-primary mt-8'>Trigger Event 2</button>
+      <button className='button button-primary mt-8' onClick={handleClick}>
+        Trigger Event 2
+      </button>
     </>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,17 @@
 function HomePage() {
+  function handleClick() {
+    window.fbq?.('trackCustom', 'Event_1_Clicked');
+  }
+
   return (
     <>
       <h2 className='mt-8 text-xl font-medium'>Home Page</h2>
       <p className='mt-2 text-neutral-800'>
         The button below is to trigger custom event
       </p>
-      <button className='button button-primary mt-8'>Trigger Event 1</button>
+      <button className='button button-primary mt-8' onClick={handleClick}>
+        Trigger Event 1
+      </button>
     </>
   );
 }
